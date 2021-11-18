3e820bfa6756c6d07901d8f6da580883c3217c9b
BryanBac <49624724+BryanBac@users.noreply.github.com>
2021-11-18 16:16:31 -0600

    inicio
    No necesita descripción

---
+++ multi-electrico/src/main/java/com/mycompany/multi/electrico/Capacitancia.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.multi.electrico;
+
+/**
+ *
+ * @author HP15DA0023LA
+ */
+public class Capacitancia {
+    
+}
